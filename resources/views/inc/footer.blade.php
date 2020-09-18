3a26c7406deb6f610b253b248b08c63d4b8c8c87
kk0055 <rtsw.lgy@gmail.com>
2020-09-18 11:41:51 +0800

    Twitter

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,6 @@
 <footer>
-  <a href="https://twitter.com/ohana_english?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @ohana_english</a>
+  <a href="https://twitter.com/malaysia_pet?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @malaysia_pet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
   
 </footer>
+
