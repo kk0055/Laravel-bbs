fe61794f839c3f9946e6ca26d8e2ad6fd2c8b8ff
kk0055 <rtsw.lgy@gmail.com>
2020-10-17 23:25:14 +0800

    tweet

--- resources/views/api/cat.blade.php
+++ resources/views/api/cat.blade.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
   
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-  <link rel="stylesheet" href="/css/api.css">
+  <link rel="stylesheet" href="{{asset('css/api.css') }}">
 </head>
 <body>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
